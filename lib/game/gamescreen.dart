75c65c82f48b60bf1eb20eff6067a1bdf9a3e36b
abday-wong <abday.hafidz23@gmail.com>
2026-02-13 11:38:14 +0700

    membuat file gamescreen

---
+++ lib/game/gamescreen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Fruit Catcher Game', home: const GameScreen());
+  }
+}
+
+class GameScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Game akan ditampilkan disini'),
+      ),
+    );
+  }
+}
